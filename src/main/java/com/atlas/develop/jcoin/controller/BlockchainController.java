72f330fd496370edf60721b020697d9fd4141482
IAmHafid <hafid.meliani@protonmail.com>
2024-08-30 00:26:04 +0200

    blockchain controller endpoints

--- src/main/java/com/atlas/develop/jcoin/controller/BlockchainController.java
+++ src/main/java/com/atlas/develop/jcoin/controller/BlockchainController.java
@@ -3,6 +3,7 @@ package com.atlas.develop.jcoin.controller;
 import com.atlas.develop.jcoin.service.BitcoinRpcService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,41 +16,45 @@ public class BlockchainController {
     @Autowired
     private BitcoinRpcService bitcoinRpcService;
 
+    // Renvoie le nombre de blocs dans la meilleure chaîne.
     @GetMapping("/getblockcount")
     public Map<String, Object> getblockcount(){
-        return null;
+        return bitcoinRpcService.callRpcMethod("getblockcount");
     }
 
-    //Renvoie le nombre de blocs dans la meilleure chaîne.
-
+    // Renvoie le hachage du meilleur bloc actuel.
     @GetMapping("/getbestblockhash")
     public Map<String, Object> getbestblockhash(){
-        return null;
+        return bitcoinRpcService.callRpcMethod("getbestblockhash");
     }
-    //: Renvoie le hachage du meilleur bloc actuel.
 
-    @GetMapping("/getblockhash")
-    public Map<String, Object> getblockhash(){
-        return null;
+
+    // Renvoie le hachage d'un bloc à un certain index.
+    @GetMapping("/getblockhash/{height}")
+    public Map<String, Object> getblockhash(@PathVariable("height") Long height) {
+        return bitcoinRpcService.callRpcMethod("getblockhash", height);
     }
-    //: Renvoie le hachage d'un bloc à un certain index.
 
-    @GetMapping("/getblock")
-    public Map<String, Object> getblock(){
-        return null;
+
+    // Renvoie les informations sur un bloc, donné son hachage.
+    @GetMapping("/getblock/{hash}")
+    public Map<String, Object> getblock(@PathVariable("hash") String hash){
+        return bitcoinRpcService.callRpcMethod("getblock", hash);
     }
-    // : Renvoie les informations sur un bloc, donné son hachage.
 
+
+    // Renvoie les informations sur tous les tips de chaînes connues.
     @GetMapping("/getchaintips")
     public Map<String, Object> getchaintips(){
         return null;
     }
-    // : Renvoie les informations sur tous les tips de chaînes connues.
 
+
+    // Renvoie la difficulté actuelle du réseau.
     @GetMapping("/getdifficulty")
     public Map<String, Object> getdifficulty(){
-        return null;
+        return bitcoinRpcService.callRpcMethod("getdifficulty");
     }
-    //: Renvoie la difficulté actuelle du réseau.
+
 
 }
